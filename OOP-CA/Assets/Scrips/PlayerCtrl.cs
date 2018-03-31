281c97420abfe1a09512d07b7d14cb221b0922ec
Sniper0000 <zhimianwuchina@gmail.com>
2018-03-31 22:28:45 +0100

    I have add some ctrl scripts to make the charactor move 3.31.2018

---
+++ OOP-CA/Assets/Scrips/PlayerCtrl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed = 6;
+    public float rotSpeed = 100;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		transform .position += transform .forward * speed * Time .deltaTime;
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * -rotSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+        }
+	}
+}
